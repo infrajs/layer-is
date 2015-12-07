483ea966aede961be6b080d519291262d8c3c70f
Антон Кияткин <a.kiyatkin@itlf.ru>
2015-12-07 19:54:55 +0400

    ns

---
+++ Is.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace infrajs\controller\ext;
+
+class is
+{
+	public function check(&$layer)
+	{
+		if (!isset($layer['is']) || is_null($layer['is'])) {
+			$is = true;
+		} else {
+			$is = $layer['is'];
+		}
+		if ($is == '0') {
+			$is = false;
+		}//В шаблоне false не удаётся вернуть
+		return $is;
+	}
+	public function istpl(&$layer)
+	{
+		$prop = 'is';
+		$proptpl = $prop.'tpl';
+		if (!isset($layer[$proptpl])) {
+			return;
+		}
+		$p = $layer[$proptpl];
+		$p = infra_template_parse(array($p), $layer);
+		$layer[$prop] = $p;
+	}
+}
